0a163854195f5697bb74c664d3f15568df64e961
niuyazhe <niuyazhe@sensetime.com>
2021-07-10 17:48:12 +0800

    init commit

---
+++ diagrams.mk
@@ -0,0 +1,26 @@
+PLANTUMLCLI := $(shell which plantumlcli)
+
+
+SOURCE := ./source
+PUMLS := $(shell find ${SOURCE} -name *.puml)
+PNGS  := $(addsuffix .puml.png, $(basename ${PUMLS}))
+EPSS  := $(addsuffix .puml.eps, $(basename ${PUMLS}))
+SVGS  := $(addsuffix .puml.svg, $(basename ${PUMLS}))
+
+%.puml.png: %.puml
+	$(PLANTUMLCLI) -t png -o $@ $<
+
+%.puml.eps: %.puml
+	$(PLANTUMLCLI) -t eps -o $@ $<
+
+%.puml.svg: %.puml
+	$(PLANTUMLCLI) -t svg -o $@ $<
+
+build: ${PNGS} ${EPSS} ${SVGS}
+
+all: build
+
+clean:
+	rm -rf \
+		$(shell find ${SOURCE} -name *.puml.svg)
+
