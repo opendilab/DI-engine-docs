e6c6b5d3710148c16077f02b91e1e5ec0a0cfc18
Xu Jingxin <sailxjx@gmail.com>
2021-12-13 21:17:49 +0800

    rename

--- source/conf.py
+++ source/conf.py
@@ -65,7 +65,7 @@ master_doc = 'index'
 if os.environ.get('READTHEDOCS') == "True":
     print("current dir", os.getcwd())
     print(os.listdir('./'))
-    os.rename("index_zh.rst", "index.rst")
+    os.rename("./index_zh.rst", "./index.rst")
     if os.environ.get("READTHEDOCS_VERSION") in ["zh"]:
         lang = os.environ.get("READTHEDOCS_VERSION")
         os.rename("index_{}.rst".format(lang), "index.rst")
