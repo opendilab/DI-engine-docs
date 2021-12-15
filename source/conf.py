f161d9fe6252a84dd6a00845c38f643ec7f36d73
Xu Jingxin <sailxjx@gmail.com>
2021-12-15 18:45:20 +0800

    feature(xjx): add distributed section (#56)
    * Init framework
    
    * Add distributed
    
    * Remove ignore png
    
    * Add gobigger zh
    
    * Fix break
    
    * Asyncio tips
    
    * en ver
    
    * Add cluster
    
    * Add gpu
    
    * Fix typo

--- source/conf.py
+++ source/conf.py
@@ -49,7 +49,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'enum_tools.autoenum',
+    'enum_tools.autoenum'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
